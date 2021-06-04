a1bc25587560dd0e99e22ce1394e7a8cf5d7ea8d
saniahsafdar <saniahsafdar@gmail.com>
2021-06-04 00:40:39 -0400

    Component made

---
+++ frontend/src/components/ShoutOutForm.tsx
@@ -0,0 +1,50 @@
+import { FormEvent, useState } from "react";
+import ShoutOut from "../model/ShoutOut";
+
+
+interface Props {
+    onSubmit: (shoutOut :ShoutOut) => void;
+}
+
+function ShoutOutForm({onSubmit}:Props) {
+    const [ to, toSet] = useState("");
+    const [from, fromSet] = useState("");
+    const [message, messageSet] = useState("");
+
+    function handleSubmit(event:FormEvent):void {
+        event.preventDefault();
+        const shoutOut: ShoutOut = {
+            to: to,
+            from: from,
+            message: message
+        }
+        onSubmit(shoutOut);
+
+        toSet("");
+        fromSet("");
+        messageSet("");
+    }
+
+        return (
+        <form className="ShoutOutForm" onSubmit={handleSubmit}>
+            <p>
+                <label htmlFor="ShoutOutForm_to">To:</label>
+                <input id="ShoutOutForm_to" value={to} onChange={e => toSet(e.target.value)} required />
+            </p>
+            <p>
+                <label htmlFor="ShoutOutForm_from">From:</label>
+                <input id="ShoutOutForm_from" value={from} onChange={e => fromSet(e.target.value)} required />
+            </p>
+            <p>
+                <label htmlFor="ShoutOutForm_message">Shout Out:</label>
+                <input id="ShoutOutForm_message" value={message} onChange={e => messageSet(e.target.value)} required />
+            </p>
+            <p>
+                <button type="submit">SHOUT IT OUT!</button>
+            </p>
+        </form>
+    )
+
+}
+
+export default ShoutOutForm;
